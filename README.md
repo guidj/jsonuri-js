3ea1d6947a9f54899451b44e924c2f62c1d368c9
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2014-11-14 02:50:40 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+#Map JSON to URI
+ 
+This script helps you convert  a JSON/JavaScript object into a HTTP GET request parameters. The reverse is also possible, i.e. convert HTTP GET parameters into a JSON/JavaScript object.
+
+
+##Usage
+
+```JavaScript
+> var testData = {
+    "name": "John",
+    "age": 31,
+    "schools": [
+        "School A", "School B", "School C"
+    ],
+    "friends": [
+        {
+            "name": "Mary",
+            "age": 27
+        },{
+            "name": "Susan",
+            "age": 35
+        }
+    ],
+    "family": {
+        "parents": [
+            {
+                "relation": "father",
+                "name": "Doe"
+            },
+            {
+                "relation": "mother",
+                "name": "Jane"
+            }
+        ],
+        "siblings": [
+            {
+                "sister1": {
+                    "name": "Ana",
+                    "age": 26,
+                    "children":[
+                        "Eric", "Lane"
+                    ]
+                }
+            }
+        ]
+    }
+};
+
+> var mappedString = mapJSONToUriParams(testData); //get non-encoded string
+```
+
+```URL
+> console.log(mappedString);
+name=John&age=31&schools[0]=School A&schools[1]=School B&schools[2]=School C&friends[0]name=Mary&friends[0]age=27&friends[1]name=Susan&friends[1]age=35&family:parents[0]relation=father&family:parents[0]name=Doe&family:parents[1]relation=mother&family:parents[1]name=Jane&family:siblings[0]sister1:name=Ana&family:siblings[0]sister1:age=26&family:siblings[0]sister1:children[0]=Eric&family:siblings[0]sister1:children[1]=Lane 
+```
+
+```JavaScript
+> var URIString = encodeURIComponent(mappedString); //get encoded string
+```
+
+```URL
+> console.log(URIString);
+name%3DJohn%26age%3D31%26schools%5B0%5D%3DSchool%20A%26schools%5B1%5D%3DSchool%20B%26schools%5B2%5D%3DSchool%20C%26friends%5B0%5Dname%3DMary%26friends%5B0%5Dage%3D27%26friends%5B1%5Dname%3DSusan%26friends%5B1%5Dage%3D35%26family%3Aparents%5B0%5Drelation%3Dfather%26family%3Aparents%5B0%5Dname%3DDoe%26family%3Aparents%5B1%5Drelation%3Dmother%26family%3Aparents%5B1%5Dname%3DJane%26family%3Asiblings%5B0%5Dsister1%3Aname%3DAna%26family%3Asiblings%5B0%5Dsister1%3Aage%3D26%26family%3Asiblings%5B0%5Dsister1%3Achildren%5B0%5D%3DEric%26family%3Asiblings%5B0%5Dsister1%3Achildren%5B1%5D%3DLane
+```
+
+```JavaScript
+> var restoredData = mapUriParamsToJSON(URIString); //takes encoded/non-encoded string
+```
+
+Feel free to use it.
+
+Cheers
---
+++ js/map.json.uri.js
@@ -0,0 +1,114 @@
+/*
+ * guidj@github
+ */
+
+function decodeUriParam(value){
+    var values = value.split("=");
+    
+    return {"key": decodeURIComponent(values[0]), "value": decodeURIComponent(values[1])};
+}
+
+function mapJSONToUriParams(data, prefix, call){
+    
+    prefix = typeof prefix !== 'undefined' ? prefix : "";
+    call = typeof call !== 'undefined' ? call : 0;
+
+    var map = [];
+    
+    if( Object.prototype.toString.call( data ) === '[object Array]' ) {
+
+        for (var ik = 0; ik < data.length; ik++){
+            map.push(mapJSONToUriParams(data[ik], prefix + "[" + ik + "]", call + 1));
+        };
+        
+    }else if ( Object.prototype.toString.call( data ) === '[object Object]' ) {
+        Object.keys(data).map(function(k){
+            var sep = "";
+            
+            //not empty
+            if (prefix !== ""){
+                
+                if (prefix.slice(-1) !== "]"){
+                    sep = ":";
+                }
+            }
+            
+            map.push(mapJSONToUriParams(data[k], prefix + sep + k, call + 1));
+        });      
+        
+    }else{
+        map.push(prefix + "=" + data);
+    }   
+        
+    return map.join("&");
+}
+
+function mapObjectKey(key, value, object){
+    
+    var indexOfObjectSep = key.indexOf(":");
+    var indexOfArray = key.indexOf("["); 
+    
+    if ((indexOfObjectSep > -1 && indexOfObjectSep < indexOfArray) || (indexOfObjectSep > -1 && indexOfArray === -1)){
+        
+        var extractedKey = key.substr(0, indexOfObjectSep);
+        var remainingKey = key.substr(indexOfObjectSep + 1);
+        
+        if (!(extractedKey in object)){
+            object[extractedKey] = {};
+        }
+        
+        if (remainingKey === ""){
+            object[extractedKey] = value;
+        }else{
+            return mapObjectKey(remainingKey, value, object[extractedKey]);
+        } 
+        
+    } else if ((indexOfArray > -1 && indexOfArray < indexOfObjectSep) || (indexOfArray > -1 && indexOfObjectSep === -1)){
+        
+        var extractedKey = key.substr(0, indexOfArray);
+        var remainingKey = key.substr(key.indexOf("]") + 1);
+        
+        if (!(extractedKey in object)){
+            object[extractedKey] = [];
+        }
+        
+        var index = parseInt(key.substr(indexOfArray + 1, key.indexOf("]") - 1));
+        
+        if (!(index in object[extractedKey])){
+            object[extractedKey][index] = {};
+        }
+        
+        if (remainingKey === ""){
+            object[extractedKey][index] = value;
+        }else{
+            return mapObjectKey(remainingKey, value, object[extractedKey][index]);
+        }   
+        
+    }else{
+        object[key] = value;
+    }
+}
+
+function mapUriParamsToJSON(data, object, call){
+    
+    call = typeof call !== 'undefined' ? call : 0;
+    object = typeof object !== 'undefined' ? object : {};
+    
+    if (call === 0){
+        data = decodeURIComponent(data).split("&");
+        
+        for (var i = 0; i < data.length; i++){
+            mapUriParamsToJSON(data[i], object, call + 1);
+        }        
+    }else{
+        //decode data
+        var pair = decodeUriParam(data);
+        //build object recursively
+        mapObjectKey(pair["key"], pair["value"], object);
+    }
+
+    return object;
+}
+
+
+
