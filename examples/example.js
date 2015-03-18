16da3700c9de2e160e996543b2b1ca9a15a16895
Guilherme Dinis Jr <guilherme.dinisjunior@gmail.com>
2015-03-19 04:05:28 +0800

    Added example html page. Closes #4

--- examples/example.js
+++ examples/example.js
@@ -38,14 +38,39 @@ var testData = {
     }
 };
 
-console.log("Original Data:");
-console.log(testData);
-var mappedData = mapJSONToUriParams(testData);
-console.log("Converted format:");
-console.log(mappedData);
-console.log("URI format");
-var uriMappedData = encodeURIComponent(mappedData);
-console.log(uriMappedData);
-console.log("An attempt are restoration");
-var recoveredData = mapUriParamsToJSON(uriMappedData);
-console.log(recoveredData);
+(function() {
+
+    var mappedData = mapJSONToUriParams(testData);
+    console.log("Test Data:");
+    console.log(testData);
+    console.log("Converted format:");
+    console.log(mappedData);
+    console.log("URI encoded");
+    var uriMappedData = encodeURIComponent(mappedData);
+    console.log(uriMappedData);
+    console.log("An attempt are restoration");
+    var recoveredData = mapUriParamsToJSON(uriMappedData);
+    console.log(recoveredData);
+
+    //console.log("Original Data:");
+    //console.log(testData);
+    
+    document.write("<h1>JSONURI: Example</h1>");
+    document.write("<p>Check console and source code</p>");
+    document.write("<h2>Test Data:</h2>");
+    document.write("<p>" + JSON.stringify(testData) + "</p>");
+
+    document.write("<h2>Serialized Data:</h2>");
+    document.write("<p>" + mappedData + "</p>");
+
+    document.write("<h2>URI Encoding:</h2>");
+    document.write("<p>" + uriMappedData + "</p>");
+
+    document.write("<h2>Recovered Data:</h2>");
+    document.write("<p>" + JSON.stringify(recoveredData) + "</p>");
+    
+    
+
+
+})();
+
